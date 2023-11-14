2b275c11d85040ad4e7b3cf0b75d4fa13560de9c
Z Wilder <zwilder@wsl-blog.com>
2023-11-13 23:09:17 -0800

    GNU GPL V3

--- src/markov_gen.c
+++ src/markov_gen.c
@@ -1,3 +1,22 @@
+/*
+* Markov Generator
+* Copyright (C) Zach Wilder 2023
+* 
+* This file is a part of Markov Generator
+*
+* Markov Generator is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* Markov Generator is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with Markov Generator.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <markov.h>
 /*****
  * Markov chain generator functions
